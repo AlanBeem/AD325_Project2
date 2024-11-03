d9b4f7c0a52a47817691576005fbf6afdb6a1dc3
AlanBeem <AlanBeem@github.com>
2024-11-03 15:50:05 -0800

    ADT Testing and Problem-specific Testing complete

--- max_heap.py
+++ max_heap.py
@@ -4,13 +4,17 @@
 
 class MaxHeap:
     def __init__(self, name: str =''):
-        self.array = self.HeapArray()  # of (priority, item)
-        self.last_child = 0
-        self.last_parent = 0
-        self.name = name
+        # self.array = self.HeapArray()  # of (priority, item)
+        self.array = []
+        # self.last_child = 0
+        # self.last_parent = 0
+        if name == '':
+            self.name = self.__repr__()
+        else:
+            self.name = name
 
     def __str__(self) -> str:
-        return str(self.array)
+        return f"{self.name}: {str(self.array)}"
 
     def parent(self, index):
         return (index - 1) // 2
@@ -23,70 +27,105 @@ class MaxHeap:
 
     def has_left(self, index):
         left_index = self.left_child(index)
-        return left_index <= self.last_child
+        # return left_index <= self.last_child
+        return left_index < len(self.array)
 
     def has_right(self, index):
         right_index = self.right_child(index)
-        return right_index <= self.last_child
+        # return right_index <= self.last_child
+        return right_index < len(self.array)
             
     def swap(self, i, j):
         self.array[i], self.array[j] = self.array[j], self.array[i]
 
     def percolate_up(self, index):
         parent_index = self.parent(index)
-        if self.array[index][0] > self.array[parent_index][0]:
+        if index > 0 and self.array[index][0] > self.array[parent_index][0]:
             self.swap(index, parent_index)
-            if parent_index > 0:
-                self.percolate_up(parent_index)
+            self.percolate_up(parent_index)
 
     def percolate_down(self, index):
-        child_indices = (self.left_child(index), self.right_child(index))
-        if child_indices[0] <= self.last_child and child_indices[1] <= self.last_child:
-            if self.array[child_indices[0]][0] >= self.array[child_indices[1]][0]:
-                child_index = child_indices[0]
-            else:
-                child_index = child_indices[1]
-            if self.array[index][0] < self.array[child_index][0]:
-                self.swap(index, child_index)
-                if child_index <= self.last_parent:
-                    self.percolate_down(child_index)  # recursive
+        child_index = index  # largest
+        # print(f"index: {index}")
+        # print(f"self.last_child: {self.last_child}")
+        # if self.has_left(index) and self.has_right(index):
+        #     if self.array[self.left_child(index)] > self.array[self.right_child(index)]:
+        #         child_index = self.left_child(index)
+        #     else:
+        #         child_index = self.right_child(index)
+        # else:
+        #     if self.has_left(index):
+        #         child_index = self.left_child(index)
+            # elif self.has_right(index):
+            #     child_index = self.right_child(index)
+        # child_indices = (self.left_child(index), self.right_child(index))
+        # if child_indices[0] <= self.last_child and child_indices[1] <= self.last_child:
+        #     if self.array[child_indices[0]][0] >= self.array[child_indices[1]][0]:
+        #         child_index = child_indices[0]
+        #     else:
+        #         child_index = child_indices[1]
+        # if len(self.array) > 0:
+        #     if self.array[index][0] < self.array[child_index][0]:
+        #         self.swap(index, child_index)
+        #         self.percolate_down(child_index)  # recursive
+        left = self.left_child(index)
+        right = self.right_child(index)
+        if self.has_left(index) and self.array[left][0] > self.array[child_index][0]:
+            child_index = left
+        if self.has_right(index) and self.array[right][0] > self.array[child_index][0]:
+            child_index = right
+        if child_index != index:
+            self.swap(index, child_index)
+            self.percolate_down(child_index)
+        
 
     def push(self, priority, item: any):
-        if len(self.array) == 0:
-            self.array[0] = (priority, item)
-        else:
-            self.array[self.last_child + 1] = (priority, item)
-            self.last_child += 1
-            self.last_parent = self.parent(self.last_child)
-            self.percolate_up(self.last_child)  # len(self.array) - 1
+        # if len(self.array) == 0:
+        #     self.array[0] = (priority, item)
+        # else:
+            # self.last_child += 1
+            # self.array[self.last_child] = (priority, item)  # this line confusing, HeapArray(list) main utility: displaying None references in tree representation ? agile development ?
+            # # self.last_parent = self.parent(self.last_child)
+            # self.percolate_up(self.last_child)  # len(self.array) - 1
+        self.array.append((priority, item))
+        self.percolate_up(len(self.array) - 1)
+
 
 
     def pop(self):
-        self.swap(0, self.last_child)
-        popped = self.array.pop()
-        self.last_child -= 1        #
-        self.percolate_down(0)      # bug fix: switched the order of these two lines; else: value of self.last_child invalidates comparison in self.percolate_down(...)  11/01/2024
+        if len(self.array) > 1:
+            # print(f"at call of pop: {self.array}")
+            # self.swap(0, self.last_child)
+            self.swap(0, len(self.array) - 1)
+            # print(f"after swap: {self.array}")
+            # popped = self.array.pop(self.last_child)
+            popped = self.array.pop()
+            # print(f"after pop: {self.array}")
+            # self.last_child -= 1        #
+            # self.last_child = max(0, self.last_child - 1)
+            # print(f"after decrementing last child: {self.array}")
+            self.percolate_down(0)      # bug fix: switched the order of these two lines; else: value of self.last_child invalidates comparison in self.percolate_down(...)  11/01/2024
+            # print(f"after percolate down: {self.array}")
+        else:
+            # popped = self.array.pop(self.last_child)
+            popped = self.array.pop()
         return popped
 
     def top_n(self, n) -> list[any]:
         """will return up to all the items in the heap, and will not raise an exception for n > number of items"""
         top_popped = []
-        if n >= len(self.array):
-            top_popped = self.array
-            self.array = []
-        else:
-            for t in range(n):
-                top_popped.append(self.pop())
+        for t in range(min(n, len(self.array))):
+            top_popped.append(self.pop())
         return top_popped
 
-    class HeapArray(list):
-        def __setitem__(self, index: int, value: any) -> None:
-            # # while index > len(self) - 1:
-            # #     self.append(None)
-            # for i in range(index + 1 - len(self)):
-            #     self.append(None)
-            # self[index] = value
-            if index >= len(self):
-                self.extend([None] * (index - len(self) + 1))
-            super().__setitem__(index, value)
+    # class HeapArray(list):
+    #     def __setitem__(self, index: int, value: any) -> None:
+    #         # # while index > len(self) - 1:
+    #         # #     self.append(None)
+    #         # for i in range(index + 1 - len(self)):
+    #         #     self.append(None)
+    #         # self[index] = value
+    #         if index >= len(self):
+    #             self.extend([None] * (index - len(self) + 1))
+    #         super().__setitem__(index, value)
 
